d5f4110f7820150b1a2e6c1c9c66b879bf96244a
Brian Illes <brianrilles@gmail.com>
2019-03-01 10:56:04 -0800

    styling

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,10 +3,15 @@ import React from "react";
 const Smurf = props => {
   return (
     <div className="Smurf">
-      <span onClick={e => props.deleteSmurf(e, props.id)}>x</span>
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      <div className="card">
+        <span onClick={e => props.deleteSmurf(e, props.id)}>x</span>
+      </div>
+
+      <div className="info">
+        <h3>{props.name}</h3>
+        <strong>{props.height} tall</strong>
+        <p>{props.age} smurf years old</p>
+      </div>
     </div>
   );
 };
