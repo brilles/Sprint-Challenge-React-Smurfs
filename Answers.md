17384477d960939b2b25cabb583e300c3292572f
Brian Illes <brianrilles@gmail.com>
2019-03-01 08:59:44 -0800

    completed stage 1

--- Answers.md
+++ Answers.md
@@ -1,6 +1,6 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
 
-Differences between client-side routing and server-side routing include where state is handled, speed, and refreshing. Client side routing handles state on the browser and is usually faster than server side routing because only particular pieces of the DOM are rendered during a view change. This is modern approach can be better because it requires less server requests but it can have a downside of loading a bit slower on the initial page load. Client-side routing doesn’t require a page reload on page change whereas server side does.
+Differences between client-side routing and server-side routing include where state is handled, speed, and refreshing. Client side routing handles state on the browser and is usually faster than server side routing because only particular pieces of the DOM are rendered during a view change. This modern approach can be better because it requires less server requests but it can have a downside of loading a bit slower on the initial load. Client-side routing doesn’t require a page reload on page change whereas server side does.
 
 2.  What does HTTP stand for?
 
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
-  }
-
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
   }
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  handleSubmit = e => {
+    this.props.addSmurf(e, this.state.name, this.state.age, this.state.height);
+    this.setState({
+      name: "",
+      age: "",
+      height: ""
+    });
+  };
+
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={this.handleSubmit}>
           <input
+            type="text"
             onChange={this.handleInputChange}
             placeholder="name"
             value={this.state.name}
             name="name"
           />
           <input
+            type="number"
             onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
           />
           <input
+            type="text"
             onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
