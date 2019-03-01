c8c6803c3bdcb1dbd13de050de1ce483de778dc6
Brian Illes <brianrilles@gmail.com>
2019-03-01 10:12:24 -0800

    added delete ability

--- village/src/App.js
+++ village/src/App.js
@@ -15,13 +15,15 @@ class App extends Component {
     };
   }
 
+  url = "http://localhost:3333/smurfs";
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
   componentDidMount() {
     axios
-      .get("http://localhost:3333/smurfs")
+      .get(this.url)
       .then(res => {
         this.setState({ smurfs: res.data });
       })
@@ -41,7 +43,7 @@ class App extends Component {
     };
 
     axios
-      .post("http://localhost:3333/smurfs", newSmurf)
+      .post(this.url, newSmurf)
       .then(res => {
         this.setState({
           smurfs: res.data
@@ -51,6 +53,18 @@ class App extends Component {
         console.log(err);
       });
   };
+
+  deleteSmurf(e, id) {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   render() {
     const { smurfs } = this.state;
@@ -75,7 +89,9 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={props => <Smurfs {...props} smurfs={smurfs} />}
+          render={props => (
+            <Smurfs {...props} smurfs={smurfs} deleteSmurf={this.deleteSmurf} />
+          )}
         />
       </div>
     );
